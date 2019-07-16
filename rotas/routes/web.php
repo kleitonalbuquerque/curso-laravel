18947ca1bf45889b6f2c43f06772139441312987
kleitonalbuquerque <kleiton2102@gmail.com>
2019-07-15 21:07:10 -0300

    Rotas com parâmetros

--- rotas/routes/web.php
+++ rotas/routes/web.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ola', function () {
+    return '<h1>Seja bem vindo!</h1>';
+});
+
+Route::get('/ola/bemvindo', function () {
+    return view('welcome');
+});
+
+Route::get('/nome/{nome}/{sobrenome}/', function ($nome, $sobrenome) {
+    return "<h1>Olá, $nome $sobrenome!</h1>";
+});
+
+Route::get('/repetir/{nome}/{n}', function($nome, $n) {
+    if(is_integer($n)) {
+        for ($i = 0; $i < $n; $i++) {
+            echo "<h1>Olá, $nome</h1>";
+        }
+    } else echo "Você não digitou um inteiro!";
+});
