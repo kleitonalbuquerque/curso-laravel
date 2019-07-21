c8832dff8e0017b8e7fd23e56886191dd105c0e7
kleitonalbuquerque <kleiton2102@gmail.com>
2019-07-21 19:06:41 -0300

    Trabalhando com rotas

--- rotas/routes/web.php
+++ rotas/routes/web.php
@@ -34,3 +34,31 @@ Route::get('/repetir/{nome}/{n}', function($nome, $n) {
         }
     } else echo "Você não digitou um inteiro!";
 });
+
+Route::get('/seunomecomregra/{nome}/{n}', function($nome, $n) {
+    for ($i = 0; $i < $n; $i++) {
+        echo "<h1>$i - Olá, $nome!</h1>";
+    }
+})->where('n','[0-9]+')->where('nome', '[a-zA-Z]+');
+
+// Rota com param opcional {nome?} '?'
+Route::get('/seunomesemregra/{nome?}', function($nome=null) {
+    if (isset($nome)) {
+        echo "<h1>Olá, $nome!</h1>";
+    } else echo "<p style='color:red;'>Você não passou nenhum nome!</p>";
+});
+
+// Agrupamento de Rotas
+Route::prefix('app')->group(function() {
+    Route::get('/', function() {
+        return "App Home";
+    });
+
+    Route::get('profile', function() {
+        return "Profile Page";
+    });
+
+    Route::get('about', function() {
+        return "About Page";
+    });
+});
